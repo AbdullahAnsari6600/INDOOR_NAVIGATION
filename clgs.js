fcc0226a7ad5f67b6389819c48757d1d55046284
AbdullahAnsari6600 <96906514+AbdullahAnsari6600@users.noreply.github.com>
2022-08-09 18:04:35 +0530

    Add files via upload

---
+++ clgs.js
@@ -0,0 +1,42 @@
+// JavaScript code
+
+let block1 = ['1001', '1002', '1003', '1004', "dean office", 'ladies room', 'lr' ]
+let block2 = ['2001', '2002', '2003', '2004'  ]
+let block3 = ['3001', '3002', '3003', '3004'  ]
+let block4 = ['4001', '4002', '4003', '4004'  ]
+let block5 = ['5001', '5002', '5003', '5004' ]
+ 
+
+
+
+
+let   search_block;                                                                                                                                                                                                                                                                                   ;
+function search() {
+    let input = document.getElementById('searchbar').value
+    input = input.toLowerCase();
+
+    let x = document.getElementsByClassName('card');
+
+    for (i = 0; i < block1.length; i++) {
+        if (block1.includes(input)) {
+            search_block = "1";
+        }
+        else {
+            search_block = "";
+            
+        }
+    }
+   
+    for (i = 0; i < x.length; i++) {
+		if (!x[i].innerHTML.toLowerCase().includes(search_block)) {
+			x[i].style.display="none";
+		}
+		else {
+			x[i].style.display="list-item";				
+		}
+	}
+
+
+}
+
+
